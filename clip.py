86c8952e6992d8284db1ca7ee87dff47a2d7bae4
Raja Raju <pgrajaraju@hotmail.com>
2023-07-26 00:22:34 +0530

    add image encoder

--- clip.py
+++ clip.py
@@ -4,7 +4,25 @@ from PIL import Image
 
 model_name = "microsoft/resnet-50"
 
-processor = AutoImageProcessor.from_pretrained(model_name)
-model = ResNetForImageClassification.from_pretrained(model_name)
+class ImageEncoder:
+    def __init__(self, model_name):
+        
+        self.processor = AutoImageProcessor.from_pretrained(model_name)
+        self.model = ResNetForImageClassification.from_pretrained(model_name)
 
+    def features(self, image_path):
+        image = Image.open(image_path)
+        inputs = self.processor(image, return_tensors='pt')
+        outputs = self.model(**inputs)
+
+        return outputs.logits
+
+if __name__ == "__main__":
+
+    model_name = "microsoft/resnet-50"
+    image_path = "image.png"
+
+    image_encoder = ImageEncoder(model_name)
+    image_features = image_encoder.features(image_path)
+    print(image_features.shape)
 
