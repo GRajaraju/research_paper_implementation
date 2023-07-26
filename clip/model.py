29916105f7d9c7218b124b4918ed555015cd625e
Raja Raju <pgrajaraju@hotmail.com>
2023-07-26 10:39:03 +0530

    initial commit

--- clip/model.py
+++ clip/model.py
@@ -1,8 +1,7 @@
 from transformers import AutoImageProcessor, ResNetForImageClassification
-from transformers import BertConfig, BertModel
+from transformers import BertTokenizer, BertModel
 from PIL import Image
 
-model_name = "microsoft/resnet-50"
 
 class ImageEncoder:
     def __init__(self, model_name):
@@ -11,11 +10,23 @@ class ImageEncoder:
         self.model = ResNetForImageClassification.from_pretrained(model_name)
 
     def features(self, image_path):
-        image = Image.open(image_path)
+        image = Image.open(image_path).convert("RGB")
         inputs = self.processor(image, return_tensors='pt')
         outputs = self.model(**inputs)
 
         return outputs.logits
+    
+class TextEncoder:
+  def __init__(self, model_name):
+
+    self.tokenizer = BertTokenizer.from_pretrained(model_name)
+    self.model = BertModel.from_pretrained(model_name)
+
+  def features(self, text):
+    inputs = self.tokenizer(text, return_tensors="pt")
+    output = self.model(**inputs)
+    return output.last_hidden_state
+
 
 if __name__ == "__main__":
 
@@ -25,4 +36,11 @@ if __name__ == "__main__":
     image_encoder = ImageEncoder(model_name)
     image_features = image_encoder.features(image_path)
     print(image_features.shape)
+
+    text_model = "bert-base-uncased"
+    text_encoder = TextEncoder(text_model)
+
+    text = "a puppy playing with a football on a green grass"
+    outputs = text_encoder.features(text)
+    print(outputs.shape)
 
